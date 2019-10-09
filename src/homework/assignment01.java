b16f301fa54d1a7af9966a63440b1a7dd186ee31
Mark Skilink <hailhth1904012@fpt.edu.vn>
2019-10-09 14:23:07 +0700

    Assinment 01

--- src/homework/assignment01.java
+++ src/homework/assignment01.java
@@ -34,7 +34,7 @@ class assignment01_b {
     public static void main(String[] args) {
         int[] fibonacci = new int[20];
         fibonacci[0] = 1;
-        fibonacci[1] = 2;
+        fibonacci[1] = 1;
         for (int i = 2; i < 20; ++i) {
             fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
         }
