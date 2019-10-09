b462aa6ae1c83575d94fa467c7f5420fcd048f8f
Mark Skilink <hailhth1904012@fpt.edu.vn>
2019-10-09 17:16:56 +0700

    Lab Session 02

--- src/homework/LabSession02.java
+++ src/homework/LabSession02.java
@@ -3,33 +3,42 @@ package homework;
 import java.util.Scanner;
 
 class SoNguyenTo {
-    private static boolean isSoNguyenTo(int x) {
+    public int a = 5;
+
+    public void isSoNguyenTo () {
+        System.out.println((checkPrime(a)) ? "a la 1 so nguyen to." : "a khong phai 1 so nguyen to.");
+    }
+
+    public void timSoNguyenToTiepTheo () {
+        System.out.printf("So nguyen to tiep theo cua %d la %d.%n", a, nextPrime(a));
+    }
+
+    public void setA() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nhap 1 so tu nhien thay cho a: ");
+        int x = scan.nextInt();
+        if ((checkPrime(x))) {
+            this.a = x;
+        } else {
+            System.out.println("Khong thay doi gia tri cua a");
+        }
+    }
+
+    private static boolean checkPrime(int x) {
         for (int i = 2; i < x/2; ++i) {
             if (x % i == 0) return false;
         }
         return true;
     }
 
-    private static int timSoNguyenToTiepTheo(int x) {
-        return (isSoNguyenTo(++x)) ? x : timSoNguyenToTiepTheo(x);
+    private static int nextPrime(int x) {
+        return (checkPrime(++x)) ? x : nextPrime(x);
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Nhap vao 1 so tu nhien:");
-        int a = scan.nextInt();
-
-        System.out.println((isSoNguyenTo(a)) ? "a la 1 so nguyen to." : "a khong phai 1 so nguyen to.");
-
-        System.out.printf("%nSo nguyen to tiep theo cua %d la %d.%n", a, timSoNguyenToTiepTheo(a));
-
-        System.out.println("Thay doi so a voi so: ");
-        int x = scan.nextInt();
-        if (isSoNguyenTo(x)) {
-            a = x;
-            System.out.printf("a da duoc gan gia tri %d", a);
-        } else {
-            System.out.println("Khong thay doi gia tri cua a");
-        }
+        SoNguyenTo so = new SoNguyenTo();
+        so.isSoNguyenTo();
+        so.timSoNguyenToTiepTheo();
+        so.setA();
     }
 }
