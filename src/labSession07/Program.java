376492dc5e1ee7f36f91ecaf7baeffa2bd5d277b
Mark Skilink <hailhth1904012@fpt.edu.vn>
2019-10-21 17:00:39 +0700

    Lab Session 07

--- src/labSession07/Program.java
+++ src/labSession07/Program.java
@@ -5,11 +5,9 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Program {
-    public ArrayList<Student> studentList;
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Program p = new Program();
+        ArrayList<Student> studentList = new ArrayList<>();
         int studentId;
         while (true) {
             System.out.println("\nMenu: \n1. Add student.\n2. Edit student by id.\n3. Delete student by id.");
@@ -17,12 +15,12 @@ public class Program {
             if (scan.hasNextInt()) {
                 switch (scan.nextInt()) {
                     case 1:
-                        p.studentList.add(new Student());
+                        studentList.add(new Student());
                         break;
                     case 2:
                         System.out.println("Nhap ma sinh vien de chinh sua: ");
                         studentId = scan.nextInt();
-                        for (Student s : p.studentList) {
+                        for (Student s : studentList) {
                             if (s.getId() == studentId) {
                                 s.setInfo();
                                 break;
@@ -33,22 +31,22 @@ public class Program {
                     case 3:
                         System.out.println("Nhap ma sinh vien de chinh xoa: ");
                         studentId = scan.nextInt();
-                        for (Student s : p.studentList) {
+                        for (Student s : studentList) {
                             if (s.getId() == studentId) {
-                                p.studentList.remove(s);
+                                studentList.remove(s);
                                 break;
                             }
                         }
                         System.out.println("Khong tim thay sinh vien.");
                         break;
                     case 4:
-                        p.studentList.sort(Comparator.comparing(Student::getGpa));
+                        studentList.sort(Comparator.comparing(Student::getGpa));
                         break;
                     case 5:
-                        p.studentList.sort(Comparator.comparing(Student::getName));
+                        studentList.sort(Comparator.comparing(Student::getName));
                         break;
                     case 6:
-                        for (Student s : p.studentList) {
+                        for (Student s : studentList) {
                             System.out.printf("Ma sinh vien: %-10dTen: %-25sTuoi: %-10dDia chi: %-30sDiem trung binh: %.1f%n",
                                     s.getId(), s.getName(), s.getAge(), s.getAddress(), s.getGpa());
                         }
@@ -57,13 +55,9 @@ public class Program {
                         return;
                 }
             } else {
-                System.out.println("Nhap sai cu phap. Thoat chuong trinh.");
-                return;
+                System.out.println("Nhap sai cu pha");
+                scan.next();
             }
         }
-    }
-
-    public Program() {
-        studentList = new ArrayList<>();
     }
 }
